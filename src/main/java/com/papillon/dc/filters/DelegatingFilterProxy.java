172d71dd5c43d1eb88b4a966ca079e6af510e494
papillon <devopreet.ch@gmail.com>
2017-07-12 14:42:51 -0500

    added spring security dependencies added security class added security bean config file

---
+++ src/main/java/com/papillon/dc/filters/DelegatingFilterProxy.java
@@ -0,0 +1,23 @@
+package com.papillon.dc.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by papillon on 7/12/2017.
+ */
+@WebFilter(filterName = "springSecurityFilterChain",urlPatterns = "/*")
+public class DelegatingFilterProxy implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}

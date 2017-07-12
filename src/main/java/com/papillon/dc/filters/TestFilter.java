6932834356ebc208672a4f32685319fac6fbad67
papillon <devopreet.ch@gmail.com>
2017-07-12 14:22:13 -0500

    added filter class for login page

---
+++ src/main/java/com/papillon/dc/filters/TestFilter.java
@@ -0,0 +1,25 @@
+package com.papillon.dc.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by papillon on 7/12/2017.
+ */
+@WebFilter(filterName = "TestFilter", urlPatterns = "/*")
+public class TestFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        System.out.println(((HttpServletRequest)req).getRequestURI().toString());
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}

b354e5bf605dc5f62616bc495209593809e97e8e
papillon <devopreet.ch@gmail.com>
2017-07-19 15:14:12 -0500

    logging home controller

--- src/main/java/com/papillon/dc/controllers/HomeController.java
+++ src/main/java/com/papillon/dc/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.papillon.dc.controllers;
 
 import com.papillon.dc.dao.Offer;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,11 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    private static Logger logger = Logger.getLogger(HomeController.class);
     @RequestMapping("/")
     public String showHome(HttpSession session, Model model){
+        logger.debug("********************showing home page**************************");
+        logger.info("********************showing home page**************************");
         return "index";
     }
 
